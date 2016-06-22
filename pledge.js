da158d0b99411fe14aa48068988e0d559b0d8600
yungtiec <tctammychu@gmail.com>
2016-06-22 18:05:08 -0400

    working on .then returning promise

--- pledge.js
+++ pledge.js
@@ -4,8 +4,68 @@ Promises Workshop: build the pledge.js deferral-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
+function $Promise () {
+    this.state = 'pending';
+    this.handlerGroups = [];
+}
+
+$Promise.prototype.then = function (succHandler, errHandler) {
+    var newDeferral = new Deferral();
+    succHandler = (typeof succHandler === 'function') ? succHandler : false;
+    errHandler = (typeof errHandler === 'function') ? errHandler : false;
+    this.handlerGroups.push({successCb:  succHandler, errorCb: errHandler, forwarder: newDeferral});
+    if (this.state !== 'pending') {
+        this.callHandlers();
+    }
+    if(!this.handlerGroups[0].successCb) return this;
+    else return this.handlerGroups[0].forwarder.$promise;
+};
+
+$Promise.prototype.catch = function (errHandler) {
+    return this.then(null, errHandler);
+};
+
+$Promise.prototype.callHandlers = function () {
+    if (this.state === 'resolved') {
+        if (!this.handlerGroups[0].successCb) return;
+        this.handlerGroups[0].successCb(this.value);
+        this.handlerGroups.shift();
+    }
+    if (this.state === 'rejected') {
+        if (!this.handlerGroups[0].errorCb) return;
+        this.handlerGroups[0].errorCb(this.value);
+        this.handlerGroups.shift();
+    }
+};
+
+function Deferral () {
+    this.$promise = new $Promise();
+}
+
+Deferral.prototype.resolve = function (data) {
+    if(this.$promise.state === 'pending') {
+        this.$promise.value = data;
+        this.$promise.state = 'resolved';
+    }
+    while (this.$promise.state !== 'pending' && this.$promise.handlerGroups.length) {
+            console.log(this.$promise.handlerGroups.length);
+            this.$promise.callHandlers();
+        }
+};
+
+Deferral.prototype.reject = function (reason) {
+    if(this.$promise.state === 'pending') {
+        this.$promise.value = reason;
+        this.$promise.state = 'rejected';
+    }
+    while (this.$promise.state !== 'pending' && this.$promise.handlerGroups.length)
+        this.$promise.callHandlers();
+};
 
 
+function defer () {
+    return new Deferral();
+}
 
 
 
--- pledge.spec.ch4.js
+++ pledge.spec.ch4.js
@@ -34,7 +34,7 @@ describe('For a given promiseA (pA)', function(){
   function thisReturnsHi () { return 'hi'; }
   function thisThrowsErr () { throw 'err'; }
 
-  xit('.then adds a new deferral to its handler group', function(){
+  it('.then adds a new deferral to its handler group', function(){
     promiseA.then();
     expect( promiseA.handlerGroups[0].forwarder instanceof Deferral ).toBe( true );
     // each handler group has its own forwarder
@@ -46,7 +46,7 @@ describe('For a given promiseA (pA)', function(){
 
   // Passing this may break your .catch from chapter 3. If that happens,
   // you will have to go back and fix .catch, taking this spec into account.
-  xit('.then returns the promise from that deferral', function(){
+  it('.then returns the promise from that deferral', function(){
     var promiseB = promiseA.then();
     expect( promiseB ).toBe( promiseA.handlerGroups[0].forwarder.$promise );
   });
@@ -54,7 +54,7 @@ describe('For a given promiseA (pA)', function(){
   describe('that returns promiseB (pB) via .then:', function(){
 
     // Resolution bubbles down to the first available success handler.
-    xit("if pA is resolved but has no success handler, pB is resolved with pA's value", function(){
+    it("if pA is resolved but has no success handler, pB is resolved with pA's value", function(){
       var promiseB = promiseA.then();
       deferralA.resolve( 9001 );
       // do not set state manually; 'resolve' should be called somewhere!
